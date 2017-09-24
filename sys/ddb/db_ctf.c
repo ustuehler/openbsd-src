b73450a8767833ab90db9a62a53cbceb3e0c7bd0
Uwe Stuehler <uwe@openbsd.org>
2017-09-24 09:21:31 +0000

    wip

--- sys/ddb/db_ctf.c
+++ sys/ddb/db_ctf.c
@@ -562,7 +562,7 @@ db_ctf_pprint_cmd(db_expr_t addr, int have_addr, db_expr_t count, char *modif)
 	 */
 	t = db_read_token();
 	if (t != tIDENT) {
-		db_printf("Bad symbol name: %s\n", db_tok_string);
+		db_printf("Bad symbol name: %s (token: %d)\n", db_tok_string, t);
 		db_flush_lex();
 		return;
 	}
